27bab364990a54b11bb739abedbb9241c8e51055
SuhaniChavan26 <suhanichavan26@gmail.com>
2025-06-23 16:36:47 +0530

    Modified

--- main.cpp
+++ main.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 using namespace std;
 
-// Function to calculate and print factorial
-void printFactorial(int n) {
-    long long factorial = 1;
-    for(int i = 1; i <= n; ++i) {
-        factorial *= i;
-    }
-    cout << "Factorial of " << n << " is " << factorial << endl;
-}
-
 int main() {
-    int number;
-    cout << "Enter a positive integer: ";
-    cin >> number;
+    int n;
+    cout << "Enter the number of terms: ";
+    cin >> n;
 
-    if (number < 0) {
-        cout << "Factorial is not defined for negative numbers." << endl;
-    } else {
-        printFactorial(number);
+    int t1 = 0, t2 = 1, nextTerm;
+
+    cout << "Fibonacci Series: ";
+
+    for (int i = 1; i <= n; ++i) {
+        cout << t1 << " ";
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
     }
 
     return 0;
