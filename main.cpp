438e113c2366a257a50ee31ea516f6a20a6235d2
SuhaniChavan26 <suhanichavan26@gmail.com>
2025-06-24 14:48:17 +0530

    Add fibonacci function

--- main.cpp
+++ main.cpp
@@ -4,10 +4,29 @@ using namespace std;
 // Function to calculate and print factorial
 void printFactorial(int n) {
     long long factorial = 1;
-    for(int i = 1; i <= n; ++i) {
+    for (int i = 1; i <= n; ++i) {
         factorial *= i;
     }
     cout << "Factorial of " << n << " is " << factorial << endl;
+}
+
+// Function to generate and print Fibonacci series up to n terms
+void printFibonacci(int n) {
+    if (n <= 0) {
+        cout << "Fibonacci series is not defined for non-positive terms." << endl;
+        return;
+    }
+
+    long long first = 0, second = 1;
+    cout << "Fibonacci series up to " << n << " terms: ";
+
+    for (int i = 1; i <= n; ++i) {
+        cout << first << " ";
+        long long next = first + second;
+        first = second;
+        second = next;
+    }
+    cout << endl;
 }
 
 int main() {
@@ -16,9 +35,10 @@ int main() {
     cin >> number;
 
     if (number < 0) {
-        cout << "Factorial is not defined for negative numbers." << endl;
+        cout << "Factorial and Fibonacci series are not defined for negative numbers." << endl;
     } else {
         printFactorial(number);
+        printFibonacci(number);
     }
 
     return 0;
